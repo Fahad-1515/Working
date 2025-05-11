94771ead6647e151fc828ae8f628dada07341f8c
Fahad-1515 <fahad630321@gmail.com>
2025-05-11 15:43:17 +0530

    2025Calender

--- python.py
+++ python.py
@@ -1,8 +1,5 @@
-income = float(input("Enter the annual income: "))
+import calendar
 
-if income < 85528:
-	tax = income * 0.18 - 556.02
-# Write the rest of your code here.
-
-tax = round(tax, 0)
-print("The tax is:", tax, "thalers")
+calendar.setfirstweekday(calendar.SUNDAY)
+calendar.prmonth(2020, 12)
+    
