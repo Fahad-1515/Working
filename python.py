0011031fb01bc512ea482e35dff0d730231fc90a
Fahad-1515 <fahad630321@gmail.com>
2025-05-16 12:10:21 +0530

    AlarmClock

--- python.py
+++ python.py
@@ -1,5 +1,12 @@
-import calendar
+class Car:
+    wheel=4
+    def __init__(self,model,year):      #constructor
+        self.model=model                   #Assign Attributes -value when fun call
+        self.year=year                     #instance variable       
+    @classmethod
+    def drive(cls):    		 #methods  are operation/fun within class
+        return f"you can drive{cls.wheel}"
 
-calendar.setfirstweekday(calendar.SUNDAY)
-calendar.prmonth(2020, 12)
-    
+car1 =Car("Lenovo",2024)                #Object
+                        #method 
+print(Car.drive())                                       
