6895a41f1ba4e93d182ad9cedfa059b1b8f0f5d1
Fahad-1515 <fahad630321@gmail.com>
2025-05-26 12:32:22 +0530

    DiceRoll

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-let rad;
-const PI = 3.14;
-let cir;
-document.getElementById("mysubmit").onclick = function () {
-  rad = document.getElementById("mytext").value;
-  rad = Number(rad);
-  cir = 2 * PI * rad;
-  document.getElementById("h1").textContent = cir + "cm";
-};
+//DICE ROLLER PROGRAM
+function rollDice() {
+  const numOfDice = document.getElementById("numOfDice").value;
+  const diceResult = document.getElementById("diceResult");
+  const diceImages = document.getElementById("diceImages");
+  const values = [];
+  const images = [];
+
+  for (let i = 0; i < numOfDice; i++) {
+    const value = Math.floor(Math.random() * 6) + 1;
+    values.push(value);
+    images.push(`<img src="/dice_images/${value}.png" alt="Dice" ${value}">`);
+  }
+  diceResult.textContent = `dice: ${values.join(", ")}`;
+  diceImages.innerHTML = images.join("");
+}
