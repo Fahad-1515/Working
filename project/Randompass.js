010c3c313049bf8d5c9a1a4121b00d8437441135
Fahad-1515 <fahad630321@gmail.com>
2025-05-26 13:48:00 +0530

    Random Password generater

---
+++ project/Randompass.js
@@ -0,0 +1,47 @@
+// RANDOM PASSWORD GENERATOR
+function generatePassword(
+  length,
+  includeLowercase,
+  includeUppercase,
+  includeNumbers,
+  includeSymbols
+) {
+  const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numberChars = "0123456789";
+  const symbolChars = "!@#$%^&*()_+-=";
+  let allowedChars = " ";
+  let password = " ";
+  allowedChars += includeUppercase ? uppercaseChars : "";
+  allowedChars += includeLowercase ? lowercaseChars : "";
+  allowedChars += includeSymbols ? symbolChars : "";
+  allowedChars += includeNumbers ? numberChars : "";
+
+  if (length <= 0) {
+    return `(password length must be atleast 1)`;
+  }
+  if (allowedChars.length === 0) {
+    return `(At least 1 set of character needs to be selected)`;
+  }
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * allowedChars.length);
+    password += allowedChars[randomIndex];
+  }
+
+  return password;
+}
+
+const passwordLength = 8;
+const includeLowercase = true;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+const password = generatePassword(
+  passwordLength,
+  includeLowercase,
+  includeUppercase,
+  includeNumbers,
+  includeSymbols
+);
+
+console.log(`Generated password:${password}`);
