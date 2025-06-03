43b3e09777d4b14faa0409ff2354323f9a720b6d
Fahad-1515 <fahad630321@gmail.com>
2025-06-03 15:55:44 +0530

    iterate object

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-//DICE ROLLER PROGRAM
-function rollDice() {
-  const numOfDice = document.getElementById("numOfDice").value;
-  const diceResult = document.getElementById("diceResult");
-  const diceImages = document.getElementById("diceImages");
-  const values = [];
-  const images = [];
+const person = {
+  firstName: "WCypher",
+  age: "23",
+  isEmployed: false,
+  sayHello: function () {
+    console.log("hi");
+  },
+  eat: () => console.log("he is eating"),
+};
 
-  for (let i = 0; i < numOfDice; i++) {
-    const value = Math.floor(Math.random() * 6) + 1;
-    values.push(value);
-    images.push(
-      `<img src="/project/dice_images/${value}.png" alt="Dice" ${value}">`
-    );
-  }
-  diceResult.textContent = `dice: ${values.join(", ")}`;
-  diceImages.innerHTML = images.join("");
+for (var prop of Object.keys(person)) {
+  console.log(`${prop}: ${person[prop]}`);
 }
